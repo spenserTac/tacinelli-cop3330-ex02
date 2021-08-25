8b1880dc7d0e5f90e0ab85de24cbf4a33271363f
spenserTac <spenserdt@gmail.com>
2021-08-24 20:07:27 -0400

    Exercise completed.

--- src/main/java/Exercise_2.java
+++ src/main/java/Exercise_2.java
@@ -1,5 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Spenser Tacinlli
+ */
+
+import java.util.Scanner;
+
 public class Exercise_2{
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is the input string? ");
+
+        String input_string = scanner.nextLine();
+        int string_count = input_string.length();
+
+        System.out.println(input_string + " has " + string_count + " characters.");
     }
 }
