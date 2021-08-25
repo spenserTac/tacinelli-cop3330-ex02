1622a320138db75431aa0c409a18515f072b4f8a
spenserTac <spenserdt@gmail.com>
2021-08-24 21:27:31 -0400

    Changed println -> print.

--- src/main/java/Exercise_2.java
+++ src/main/java/Exercise_2.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Exercise_2{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What is the input string? ");
+        System.out.print("What is the input string? ");
 
         String input_string = scanner.nextLine();
         int string_count = input_string.length();
